5809f4853781595e8d78ce86051439293c4264db
VirajxD0 <virajvd99@gmail.com>
2026-01-31 13:12:31 +0530

    Routes added

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import { config } from "dotenv";
-// import morgan from "morgan";
-// import appRouter from "./routes/index.js";
+import morgan from "morgan";
+import appRouter from "./routes/index.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 config();
@@ -13,8 +13,8 @@ app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
 //remove it in production
-// app.use(morgan("dev"));
+app.use(morgan("dev"));
 
-// app.use("/api/v1", appRouter);
+app.use("/api/v1", appRouter);
 
 export default app;
