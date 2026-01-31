15f849386559fa7e71d9366708dfe25b19aea598
VirajxD0 <virajvd99@gmail.com>
2026-01-31 11:40:04 +0530

    second commit

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+import express from "express";
+const app = express();
+app.use(express.json());
+app.post("/hello", (req, res, next) => {
+    console.log(req.body.name);
+    return res.send("Hello");
+});
+app.listen(5500, () => console.log("Server is running on http://localhost:5500"));
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
-console.log(
-  "This is a starter kit for this amazing project. With 💓 By Indian Coders"
+import express from "express";
+
+const app = express();
+app.use(express.json());
+app.post("/hello", (req, res, next) => {
+  console.log(req.body.name);
+
+  return res.send("Hello");
+});
+
+app.listen(5500, () =>
+  console.log("Server is running on http://localhost:5500"),
 );
